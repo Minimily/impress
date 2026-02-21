f8eedfab846813cfd4b4a498116cfda31f9430c5
Hildeberto Mendonca <me@hildeberto.com>
2026-02-21 11:37:43 -0500

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::fs;
+
+use typst_pdf::PdfOptions;
+use gorgeous::TypstWrapperWorld;
+
+fn main() {
+    let content = r#"
+#import "@preview/polylux:0.4.0": *
+
+#set page(paper: "presentation-16-9")
+
+
+#set text(
+  font: "Lato",
+  size: 23pt,
+)
+
+#slide[
+  #set page(footer: none)
+  #set align(horizon + center)
+
+= Hello, World!
+A document (+ `polylux` library) rendered with `Typst`!
+$ y = m x + n $
+]"#
+        .to_owned();
+
+    // Create world with content.
+    let world = TypstWrapperWorld::new("../".to_owned(), content);
+
+    // Render document
+    let document = typst::compile(&world)
+        .output
+        .expect("Error compiling typst");
+
+    // Output to pdf and svg
+    let pdf = typst_pdf::pdf(&document, &PdfOptions::default()).expect("Error exporting PDF");
+    fs::write("./output.pdf", pdf).expect("Error writing PDF.");
+    println!("Created pdf: `./output.pdf`");
+}
